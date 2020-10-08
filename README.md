46227ae5cd3203c2d81906c42176f4c59bc0d7dd
mahme <mustafasaleh.hf@gmail.com>
2020-10-08 09:47:05 +0200

    Geolocation disabled

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # OpenWeather App
 
 - This app is a cloned version of the result for [weather search](https://www.bing.com/search?q=weather) in bing search engine.
+- The App fetches data from [OpenWeather map Api](https://openweathermap.org/api) to display weather information for five days, it consumes the following two apis endpoints:
+  - 5 Day / 3 Hour Forecast
+  - One Call Api
+- [React-bootstrap](https://react-bootstrap.github.io/) is used to build weather tabs and other additional componenets in app.
+- Font-awesome icons are used for search bar, while weather icons are copied from bing weather search result.
+- To display the 3-hours detailed weather of the day, [Charts.js](https://www.chartjs.org/) library is used to plot a line chart in every weather tab.
+- The app also integrates with google map javascript api to allow user to fetch weather by map location.
 - This is a link for a [live demo](https://mustafa-saleh.github.io/weather-app/).
--- src/application/App.js
+++ src/application/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Container } from "react-bootstrap";
 import Weather from "../application/Weather";
 import AppTitle from "../components/AppTitle";
@@ -15,17 +15,17 @@ function App() {
   const [error, setError] = useState({});
   const [weather, setWeather] = useState({});
 
-  useEffect(() => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(async function (position) {
-        const coords = {
-          lat: position.coords.latitude,
-          lon: position.coords.longitude,
-        };
-        fetchWeather(coords);
-      });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(function (position) {
+  //       const coords = {
+  //         lat: position.coords.latitude,
+  //         lon: position.coords.longitude,
+  //       };
+  //       fetchWeather(coords);
+  //     });
+  //   }
+  // }, []);
 
   const fetchWeather = (coords, city = null) => {
     setLoading(true);
